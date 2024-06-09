6a0b5ab4c962ee1979703857863282df71f57d20
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-06-09 16:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let loginForm = document.getElementById('login-form');
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let remember = document.getElementById('checkbox');
+let existingUserButton = document.getElementById('existing');
+
+loginForm.addEventListener('click', function(e) {
+	e.preventDefault();
+	if(remember.checked) {
+		localStorage.setItem('username', username.value);
+		localStorage.setItem('password', password.value);
+	} else {
+		localStorage.removeItem('username');
+		lo
+	}
+})
